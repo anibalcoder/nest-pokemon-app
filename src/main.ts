cee8be870cd4b0329e50ba0d1f2a29f28c55f5ce
anibalcoder <144500768+anibalcoder@users.noreply.github.com>
2026-02-25 09:03:01 -0500

    seccion-7: POST - Recibir y validar la data

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // setGlobalPrefix permite establecer un prefijo global para todas las rutas (endpoints) de la aplicación.
   app.setGlobalPrefix('api/v2');
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
 
   await app.listen(process.env.PORT ?? 3000);
 }
