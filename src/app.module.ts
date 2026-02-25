ea4a3a318ad8d1b8bfbbb630578f13f35bb0eaec
anibalcoder <144500768+anibalcoder@users.noreply.github.com>
2026-02-25 07:27:25 -0500

    seccion-7: Global Prefix

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@
 import { join } from 'path';
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
+import { PokemonModule } from './pokemon/pokemon.module';
 
 @Module({
   imports: [
@@ -15,6 +16,7 @@ import { ServeStaticModule } from '@nestjs/serve-static';
        */
       rootPath: join(__dirname, '..', 'public'),
     }),
+    PokemonModule,
   ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // setGlobalPrefix permite establecer un prefijo global para todas las rutas (endpoints) de la aplicación.
+  app.setGlobalPrefix('api/v2');
+
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+void bootstrap();
