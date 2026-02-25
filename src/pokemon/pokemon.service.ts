dc1cc5eb9ebb42679131ee3b5f07724255f555db
anibalcoder <144500768+anibalcoder@users.noreply.github.com>
2026-02-25 09:18:53 -0500

    seccion-7: Crear Pokémon en base de datos

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -1,12 +1,30 @@
 import { Injectable } from '@nestjs/common';
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
+import { Model } from 'mongoose';
+import { Pokemon } from './entities/pokemon.entity';
+import { InjectModel } from '@nestjs/mongoose';
 
 @Injectable()
 export class PokemonService {
-  create(createPokemonDto: CreatePokemonDto) {
+  constructor(
+    // Inyecta el modelo de Mongoose registrado con el nombre "Pokemon".
+    @InjectModel(Pokemon.name)
+    /**
+     * Modelo de Mongoose que representa la colección (Pokemon)
+     * y expone métodos como create(), find(), save(), etc.
+     */
+    private readonly pokemonModel: Model<Pokemon>,
+  ) {}
+
+  async create(createPokemonDto: CreatePokemonDto) {
+    // Normaliza el nombre a minúsculas antes de guardarlo
     createPokemonDto.name = createPokemonDto.name.toLocaleLowerCase();
-    return createPokemonDto;
+
+    // Inserta un nuevo documento en MongoDB usando el modelo
+    const pokemon = await this.pokemonModel.create(createPokemonDto);
+
+    return pokemon;
   }
 
   findAll() {
