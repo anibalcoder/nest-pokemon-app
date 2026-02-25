058cb9872bc28f5ea2f4066e2b0676ddc8db9c9e
anibalcoder <144500768+anibalcoder@users.noreply.github.com>
2026-02-25 07:21:46 -0500

    seccion-7: Servir contenido estático

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,20 @@
+// los paquetes que son de node van al inicio
+import { join } from 'path';
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
 
 @Module({
-  imports: [],
+  imports: [
+    // Configura el módulo para servir archivos estáticos
+    ServeStaticModule.forRoot({
+      /**
+       * Define la ruta física donde están los archivos estáticos.
+       * __dirname → carpeta actual del archivo compilado.
+       * '..' → sube un nivel en el directorio.
+       * 'public' → carpeta donde están los archivos a exponer.
+       */
+      rootPath: join(__dirname, '..', 'public'),
+    }),
+  ],
 })
 export class AppModule {}
