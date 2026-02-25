55efbc432c626915f7df851b09be5c3aa6102e4c
anibalcoder <144500768+anibalcoder@users.noreply.github.com>
2026-02-25 08:41:11 -0500

    seccion-7: Crear esquemas y Modelos

--- src/pokemon/entities/pokemon.entity.ts
+++ src/pokemon/entities/pokemon.entity.ts
@@ -1,1 +1,21 @@
-export class Pokemon {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+@Schema() // Decorador que indica que esta clase es un esquema de Mongoose
+export class Pokemon extends Document {
+  // Document representa un documento real de MongoDB y agrega métodos como save().
+
+  @Prop({
+    unique: true, // No se pueden repetir nombres
+    index: true, // Crea un índice para mejorar la búsqueda por nombre
+  })
+  name: string;
+
+  @Prop({
+    unique: true,
+    index: true,
+  })
+  no: number;
+}
+
+export const PokemonSchema = SchemaFactory.createForClass(Pokemon); // Crea el esquema a partir de la clase Pokemon
--- src/pokemon/pokemon.module.ts
+++ src/pokemon/pokemon.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { PokemonService } from './pokemon.service';
 import { PokemonController } from './pokemon.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Pokemon, PokemonSchema } from './entities/pokemon.entity';
 
 @Module({
   controllers: [PokemonController],
   providers: [PokemonService],
+  imports: [
+    // forFeature() -> registra modelos dentro de un módulo.
+    MongooseModule.forFeature([
+      {
+        /**
+         * Ese name No tiene relación con Pokemon.name.
+         * Pokemon.name es una propiedad nativa de JavaScript que devuelve el nombre de la clase (Pokemon.name → string "Pokemon").
+         */
+        name: Pokemon.name,
+        schema: PokemonSchema,
+      },
+    ]),
+  ],
 })
 export class PokemonModule {}
+
+// Name sale del documento
